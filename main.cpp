0c7b5f55ea4c9ad9da08064d4a1426cc0b903cc7
YupiSoftwarePunk <youngperchik2404@gmail.com>
2025-01-20 15:34:37 +0500

    edited some operators and added constructors

--- main.cpp
+++ main.cpp
@@ -5,7 +5,26 @@ class Integer
 {
 public:
 
+	// constructors
+	Integer():Integer(false, 0u) { }
+	Integer(bool sign, unsigned units):sign_(sign), units_(units){ }
+	Integer(int number) 
+	{
+		if (number < 0)
+		{
+			sign_ = true;
+			units_ = ::abs(number);
+		}
+		else
+		{
+			sign_ = false;
+			units_ = static_cast <unsigned> (number);
+		}
+	}
+
+
 	// number check
+
 	bool isOdd()const
 	{
 		return units_ % 2 == 0;
@@ -32,7 +51,7 @@ public:
 		return true;
 	}
 
-	bool is�oprime(Integer other)const
+	bool isCoprime(Integer other)const
 	{
 		return std::gcd(units_, other.units_) == 1;
 	}
@@ -49,14 +68,46 @@ public:
 
 
 	// operators
-	int operator+(const Integer other)const
+
+	Integer operator+(const Integer &other)const
 	{
-		return units_ + other.units_;
+		Integer result;
+		if (sign_ == other.sign_) 
+		{
+			result.sign_ = sign_;
+			result.units_ = units_ + other.units_;
+		}
+		else
+		{
+			if (units_ > other.units_)
+			{
+				result.sign_ = sign_;
+				result.units_ = units_ - other.units_;
+			}
+			else if (units_ < other.units_) 
+			{
+				result.sign_ = other.sign_;
+				result.units_ = other.units_ - units_;
+			}
+			else 
+			{
+				result.sign_ = false;
+				result.units_ = 0;
+			}
+		}
+		return result;
+	}
+
+	friend Integer operator-(const Integer other)
+	{
+		// доделать по принципу operator+
 	}
 
 
 
+
 private:
-	int units_;
+	bool sign_;
+	unsigned int units_;
 
 };
