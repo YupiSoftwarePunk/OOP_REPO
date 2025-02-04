8491e2cba2c4231d375e2b1bfb7dafb0504cc55c
YupiSoftwarePunk <youngperchik2404@gmail.com>
2025-02-04 11:38:06 +0500

    Added files with class Fraction and some checks

---
+++ Fraction.cpp
@@ -0,0 +1,74 @@
+#include "Fraction.hpp"
+
+
+// constructors
+Fraction::Fraction() : Fraction(false, Integer(1)) {}
+Fraction::Fraction(bool sign, Integer num) :sign_(sign), num_(num){}
+Fraction::Fraction(Integer number) 
+{
+	if (number < 0)
+	{
+		sign_ = true;
+		this->num_ = number;
+		this->denum_ = static_cast<Integer>(1);
+	}
+	else
+	{
+		sign_ = false;
+		this->num_ = number;
+		this->denum_ = static_cast<Integer>(1);
+	}
+}
+
+
+//setters
+void Fraction::SetSign(bool sign)
+{
+	this->sign_ = sign;
+}
+
+void Fraction::SetNumerator(Integer num)
+{
+	this->num_ = num;
+}
+
+void Fraction::SetDenominator(Integer denum)
+{
+	this->denum_ = denum;
+}
+
+
+// getters
+bool Fraction::GetSign()
+{
+	return sign_;
+}
+
+Integer Fraction::GetNumerator()
+{
+	return num_;
+}
+
+Integer Fraction::GetDenominator()
+{
+	return denum_;
+}
+
+
+// // Проверка дроби
+bool Fraction::isNumPositive() const
+{
+	return sign_ < 1;
+}
+
+bool Fraction::isCorrect() const
+{
+	return num_ < denum_;
+}
+
+bool Fraction::isSame() const
+{
+	return num_ == denum_;
+}
+
+
---
+++ Fraction.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Integer.hpp"
+
+class Fraction
+{
+public:
+	Fraction();
+	Fraction(bool sign, Integer num);
+	Fraction(Integer number);
+
+	// setters
+	void SetSign(bool sign);
+	void SetNumerator(Integer num);
+	void SetDenominator(Integer denum);
+
+	// getters
+	bool GetSign();
+	Integer GetNumerator();
+	Integer GetDenominator();
+
+	// Проверка дроби
+	bool isCorrect()const;
+	bool isNumPositive()const;
+	bool isSame()const;
+
+
+	// поля
+private:
+	bool sign_;
+	Integer num_;
+	Integer denum_;
+};
+
