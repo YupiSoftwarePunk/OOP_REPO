5ab09cf70168a36765e80c961a77d3d7e1bc7ecf
Yupi <youngperchik2404@gmail.com>
2025-02-08 14:27:51 +0500

    Classes divided into folders

---
+++ Fraction/Fraction.hpp
@@ -0,0 +1,104 @@
+#pragma once
+
+#include "Integer.hpp"
+#include <cmath>
+#include <Windows.h>
+
+class Fraction
+{
+public:
+	// constructors
+	Fraction();
+	Fraction(bool sign, Integer num, Integer denum);
+	Fraction(Integer number);
+
+	// setters
+	void SetSign(bool sign);
+	void SetNumerator(Integer num);
+	void SetDenominator(Integer denum);
+
+	// getters
+	bool GetSign();
+	Integer GetNumerator();
+	Integer GetDenominator();
+
+	// операторо вывода
+	friend std::ostream& operator<<(std::ostream &out, const Fraction &obj);
+
+
+	// Проверка дроби
+
+	// Правильная ли дробь
+	bool isProper()const;
+
+	// Неправильная ли дробь
+	bool isImProper()const;
+
+	// Положительная ли дробь
+	bool isNumPositive()const;
+
+	// Отрицательная ли дробь
+	bool isNumNegative()const;
+
+	// являются ли 2 числа одним и тем же объектом в программе
+	bool isSame(const Fraction &other)const;
+
+	// Проверка на эквивалентность
+	bool isEqual(const Fraction &obj, const Fraction &other);
+
+
+	// Методы для получения значений
+
+	// сокращенная дробь
+	Fraction ReduceFraction();
+
+	// обратная дробь
+	Fraction ReverseFraction();
+
+	// Счет целых чисел в дроби
+	Integer CountUnits() const;
+
+	// Счет остатка от целового числа в дроби
+	// сделать
+
+
+	// Арифметические операции
+	Fraction operator+(const Fraction& other);
+
+	Fraction operator-(const Fraction& other);
+
+	Fraction operator*(const Fraction& other);
+
+	Fraction operator/(const Fraction& other);
+
+
+	// Операторы сравнения
+	bool operator==(const Fraction& other) const;
+
+	bool operator!=(const Fraction& other) const;
+
+	bool operator<(const Fraction& other) const;
+
+	bool operator>(const Fraction& other) const;
+
+	bool operator<=(const Fraction & other) const;
+
+	bool operator>=(const Fraction& other) const;
+
+
+	// Операторы присваивания
+	Fraction operator+=(Fraction& other);
+
+	Fraction operator-=(Fraction& other);
+
+	Fraction operator*=(Fraction& other);
+
+	Fraction operator/=(Fraction& other);
+
+
+	// поля
+private:
+	bool sign_;
+	Integer num_;
+	Integer denum_;
+};
