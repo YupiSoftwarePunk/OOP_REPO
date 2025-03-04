3ce1b214d228df366bc1592a3e430a23cee1af20
YupiSoftwarePunk <youngperchik2404@gmail.com>
2025-03-04 10:36:33 +0500

    Realized setters, getter and operator<<

--- Real/Real.cpp
+++ Real/Real.cpp
@@ -1,1 +1,31 @@
 #include "Real.hpp"
+
+//setters
+void Real::SetUnits(Integer units)
+{
+	this->units_ = units;
+}
+
+void Real::SetFractional(Fraction fractional)
+{
+	this->fractional_ = fractional;
+}
+
+
+//getters
+Integer Real::GetUnits()
+{
+	return units_;
+}
+
+Fraction Real::GetFractional()
+{
+	return fractional_;
+}
+
+
+// Оператор вывода
+std::ostream& operator<<(std::ostream& out, const Real& obj)
+{
+	return out << obj.units_ << '.' << obj.fractional_;
+}
