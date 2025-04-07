5e160b831937cc65a6fee020f4d1368a0c559adc
Yupi <youngperchik2404@gmail.com>
2025-04-07 12:38:59 +0500

    all constructors are implemented and setters and getters are declared

--- Segment/Segment.hpp
+++ Segment/Segment.hpp
@@ -4,6 +4,52 @@
 
 class Segment
 {
+public:
+
+	// конструкторы
+	Segment(Point2d pointer1, Point2d pointer2) : pointer1_(std::make_shared<Point2d>(pointer1)), pointer2_(std::make_shared<Point2d>(pointer2)) {}
+	Segment() : pointer1_(nullptr), pointer2_(nullptr) {}
+
+	// конструктор копирования
+	Segment(const Segment& other) : pointer1_(other.pointer1_), pointer2_(other.pointer2_) {}
+
+	// конструктор присваивания
+	Segment& operator=(const Segment& other)
+	{
+		if (this != &other)
+		{
+			pointer1_ = other.pointer1_;
+			pointer2_ = other.pointer2_;
+			return *this;
+		}
+	}
+
+
+	// сеттеры
+	void SetPoiner1(std::shared_ptr<Point2d> pointer1);
+	void SetPoiner2(std::shared_ptr<Point2d> pointer2);
+
+
+	// геттеры указателя
+	std::shared_ptr<Point2d> GetPointer1();
+	std::shared_ptr<const Point2d> GetPointer1() const;
+
+	std::shared_ptr<Point2d> GetPointer2();
+	std::shared_ptr<const Point2d> GetPointer2() const;
+
+
+	// геттеры значений
+	Point2d& GetPointer1Ref();
+	const Point2d& GetPointer1Ref() const;
+
+	Point2d& GetPointer2Ref();
+	const Point2d& GetPointer2Ref() const;
+
+
+	// поля
+private:
+	std::shared_ptr<Point2d> pointer1_;
+	std::shared_ptr<Point2d> pointer2_;
 
 };
 
