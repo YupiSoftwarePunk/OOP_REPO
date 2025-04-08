e438abf3a5b2db8431e4c90c2d68d463cc07a889
Yupi <youngperchik2404@gmail.com>
2025-04-08 16:46:28 +0500

    Finished all operators and methods in Linked list class

--- LinkedList/List.cpp
+++ LinkedList/List.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 // Шаблоны классов
 // 
 //  В с++ можно сделать шаблонами:
@@ -85,32 +87,206 @@ public:
     }
 
 
+
     //operators
-    List& operator=(const List& other);
-    List& operator=(List&& other);
-    Type& operator[](size_t index);
-    const Type& operator[](size_t index) const;
+    List& operator=(const List& other)
+    {
+        if (this != &other) 
+        {
+            auto current = other.start_;
+            while (current != nullptr) 
+            {
+                push_back(current->getData());
+                current = current->getNext();
+            }
+        }
+        return *this;
+    }
+
+    List& operator=(List&& other) noexcept
+    {
+        if (this != &other) 
+        {
+            
+            start_ = other.start_;
+            end_ = other.end_;
+            size_ = other.size_;
+            other.start_ = nullptr;
+            other.end_ = nullptr;
+            other.size_ = 0;
+        }
+        return *this;
+    }
+
+    Type& operator[](size_t index)
+    {
+        Node* res = start_;
+        for (; index != 0; index--)
+        {
+            res = res->getNext();
+        }
+        return res->getData();
+    }
+
+    const Type& operator[](size_t index) const
+    {
+        Node* res = start_;
+        for (; index != 0; index--)
+        {
+            res = res->getNext();
+        }
+        return res->getData();
+    }
 
     //--------------------
 
-    Type& at(size_t index);
-    const Type& at(size_t index) const;
+    Type& at(size_t index)
+    {
+        if (index >= size_)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        Node* current = start_;
+
+        for (size_t i = 0; i < index; i++)
+        {
+            current = current->getNext();
+        }
+        return current->getData();
+    }
+
+    const Type& at(size_t index) const
+    {
+        if (index >= size_)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        Node* current = start_;
+
+        for (size_t i = 0; i < index; i++)
+        {
+            current = current->getNext();
+        }
+        return current->getData();
+    }
 
     //technicaly at is operator replacement for cases of managemant with pointer
     //=====================
     //short acces
 
     Type& back() { return end_->getData(); }
-    Type& front() {return start_->getData() }
+    Type& front() { return start_->getData(); }
 
     const Type& back()const { return end_->getData(); }
-    const Type& front()const { return start_->getData() }
-    void push_back(const Type& other);
-    void push_front(const Type& other);
-    void pop_back();
-    void pop_front();
-    void insert(const Type& other, size_t index);
-    void erase(size_t index);
+    const Type& front()const { return start_->getData(); }
+
+    void push_back(const Type& other)
+    {
+        Node* res = new Node{ other };
+        if (start_ == end_ && start_ == nullptr && end_ == nullptr)
+        {
+            start_ = res;
+        }
+        else
+        {
+            end_->setNext(res);
+            res->setPrev(end_);
+        }
+        end_ = res;
+        size_ += 1;
+    }
+
+    void push_front(const Type& other)
+    {
+        Node* res = new Node{ other };
+        if (start_ == end_ && start_ == nullptr && end_ == nullptr)
+        {
+            start_ = res;
+        }
+        else
+        {
+            start_->setNext(res);
+            res->setPrev(start_);
+        }
+        start_ = res;
+        size_ += 1;
+    }
+
+    void pop_back()
+    {
+        end_ = end_->getPrev();
+        size_ -= 1;
+        delete end_->getNext();
+        end_->setNext(nullptr);
+    }
+
+    void pop_front()
+    {
+        start_ = start_->getNext();
+        size_ -= 1;
+        delete start_->getPrev();
+        start_->setPrev(nullptr);
+    }
+
+    void insert(const Type& other, size_t index)
+    {
+        if (index > size_)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        if (index == 0) 
+        {
+            push_front(other);
+        }
+        else if (index == size_) 
+        {
+            push_back(other);
+        }
+        else
+        {
+            Node* current = start_;
+            for (size_t i = 0; i < index; ++i)
+            {
+                current = current->getNext();
+            }
+
+            Node* res = new Node(res, current, current->getPrev());
+            current->getPrev()->setNext(res);
+            current->setPrev(res);
+
+            ++size_;
+        }
+    }
+
+    void erase(size_t index)
+    {
+        if (index >= size_)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        if (index == 0) 
+        {
+            pop_front();
+        }
+        else if (index == size_ - 1) 
+        {
+            pop_back();
+        }
+        else 
+        {
+            Node* toDelete = start_;
+            for (size_t i = 0; i < index; ++i)
+            {
+                toDelete = toDelete->getNext();
+            }
+            toDelete->getPrev()->setNext(toDelete->getNext());
+            toDelete->getNext()->setPrev(toDelete->getPrev());
+
+            delete toDelete;
+            --size_;
+        }
+    }
+
 
     size_t size()const { return size_; }
 
@@ -169,89 +345,11 @@ private:
 using MyType = double;
 typedef double Duration;
 
-#include <iostream>
+
 
 int main12()
 {
-    
-}
 
 
-
-// method's realizations
-template<typename Type>
-List<Type>& List<Type>::operator=(const List& other)
-{
-    for (int i = 0; i < size; i++)
-    {
-        if (start_ == other.start_ && end_ == other.end_)
-    }
-    return *this;
-}
-
-template<typename Type>
-Type& List<Type>::operator[](size_t index)
-{
-    Node* res = start_;
-    for (; index != 0; index--)
-    {
-        res = res->getNext();
-    }
-    return res->getData();
-}
-
-
-
-template<typename Type>
-void List<Type>::push_back(const Type& other)
-{
-    Node* res = new Node{ other };
-    if (start_ == end_ && start_ == nullptr && end_ == nullptr)
-    {
-        start_ = res;
-    }
-    else
-    {
-        end_->setNext(res);
-        res->setPrev(end_);
-    }
-    end_ = res;
-    size_ += 1;
-}
-
-template<typename Type>
-void List<Type>::push_front(const Type& other)
-{
-    Node* res = new Node{ other };
-    if (start_ == end_ && start_ == nullptr && end_ == nullptr)
-    {
-        start_ = res;
-    }
-    else
-    {
-        start_->setNext(res);
-        res->setPrev(start_);
-    }
-    start_ = res;
-    size_ += 1;
-}
-
-
-
-template<typename Type>
-void List<Type>::pop_back()
-{
-    end_ = end_->getPrev();
-    size_ -= 1;
-    delete end_->getNext();
-    end_->setNext(nullptr);
-}
-
-template<typename Type>
-void List<Type>::pop_front()
-{
-    start_ = start_->getNext();
-    size_ -= 1;
-    delete start_->getPrev();
-    start_->setPrev(nullptr);
+    return 0;
 }
