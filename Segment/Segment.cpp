0819bf6f1d2f107ddd1c8f35984bd352c7a0c66f
Yupi <youngperchik2404@gmail.com>
2025-04-14 18:09:28 +0500

    Stearted IsSegmentsCrossed in Segment class

--- Segment/Segment.cpp
+++ Segment/Segment.cpp
@@ -50,6 +50,37 @@ bool Segment::IsEqualSegments(const Segment& other)
 	return *this == other;
 }
 
+bool Segment::IsSegmentsCrossed(const Segment& other)
+{
+	Point2d p1 = *pointer1_;
+	Point2d p2 = *pointer2_;
+
+	Point2d p3 = *(other.pointer1_);
+	Point2d p4 = *(other.pointer2_);
+
+
+	auto orientation = [](const Point2d& a, const Point2d& b, const Point2d& c) 
+	{
+		double orientation = (b.num2_ - a.num2_) * (c.num1_ - b.num1_) - (b.num1_ - a.num1_) * (c.num2_ - b.num2_);
+
+		if (orientation == 0)
+		{
+			return 0;
+		} 
+		else 
+		{
+			return (orientation > 0) ? 1 : 2;
+		}
+	};
+
+
+	auto onSegment = [](const Point2d& p, const Point2d& q, const Point2d& r) 
+	{
+		return q.num1_ <= std::max(p.num1_, r.num1_) && q.num1_ >= std::min(p.num1_, r.num1_) &&
+			q.num2_ <= std::max(p.num2_, r.num2_) && q.num2_ >= std::min(p.num2_, r.num2_);
+	};
+}
+
 
 
 Real GetSegmentDistance(const Point2d& num1, const Point2d& num2)
