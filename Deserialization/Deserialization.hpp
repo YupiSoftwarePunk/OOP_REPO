dbb7e684aa28e8d84e6e4853c7b9f800a7e58b09
Yupi <youngperchik2404@gmail.com>
2025-05-27 16:17:30 +0500

    Implemented serialization methods, but it needs to find correct type of number

--- Deserialization/Deserialization.hpp
+++ Deserialization/Deserialization.hpp
@@ -12,13 +12,43 @@ class Deserialization
 {
 public:
 
+    // Тут определяется формат десериализации
 	static T Deserialize(const std::string& data, const std::string& format);
 
 private:
 
+    // txt
 	static T DeserializeFromTxt(const std::string& data);
 
+    // json
 	static T DeserializeFromJson(const std::string& data);
 
+    // xml
 	static T DeserializeFromXml(const std::string& data);
 };
+
+
+
+
+// Тут определяется формат десериализации
+template<typename T>
+
+inline T Deserialization<T>::Deserialize(const std::string& data, const std::string& form)
+{
+    if (form == "txt")
+    {
+        return DeserializeFromTxt(data);
+    }
+    else if (form == "json")
+    {
+        return DeserializeFromJson(data);
+    }
+    else if (form == "xml")
+    {
+        return DeserializeFromXml(data);
+    }
+    else
+    {
+        throw std::invalid_argument("Unsupported format: " + form);
+    }
+}
--- Serialization/Serialization.hpp
+++ Serialization/Serialization.hpp
@@ -6,6 +6,12 @@
 #include "../Point2d/Point2d.hpp"
 #include "../Segment/Segment.hpp"
 
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+
 template <typename T>
 
 class Serialization
@@ -13,18 +19,82 @@ class Serialization
 public:
 
 	// Тут определяется формат сериализации 
-	static std::string Serialize(const T& num, const std::string& form);
+	static std::string Serialize(const T& num, const std::string& form)
+	{
+		if (form == "txt")
+		{
+			return SerializeToTxt(num);
+		}
+		else if (form == "json")
+		{
+			return SerializeToJson(num);
+		}
+		else if (form == "xml")
+		{
+			return SerializeToXml(num);
+		}
+		else
+		{
+			throw std::invalid_argument("Unsupported format: " + form);
+		}
+	}
+
 
 private:
 
 	// txt
-	static std::string SerializeToTxt(const T& num);
+	static std::string SerializeToTxt(const T& num)
+	{
+		std::ostringstream oss;
+		oss << num;
+		return oss.str();
+	}
+
 
 	// json
-	static std::string SerializeToJson(const T& num);
+	static std::string SerializeToJson(const T& num)
+	{
+		std::ostringstream oss;
+		oss << "{\"type\":\"" << typeName() << "\",\"value\":\"" << num << "\"}";
+		return oss.str();
+	}
+
 
 	// xml
-	static std::string SerializeToXML(const T& num);
+	static std::string SerializeToXml(const T& num)
+	{
+		std::ostringstream oss;
+		oss << "<" << typeName() << ">" << num << "</" << typeName() << ">";
+		return oss.str();
+	}
+
+
+
+	// Определение типа
+	static const char* typeName() 
+	{
+		if constexpr (std::is_same_v<T, Integer>)
+		{
+			return "Integer";
+		}
+		else if constexpr (std::is_same_v<T, Fraction>)
+		{
+			return "Fraction";
+		}
+		else if constexpr (std::is_same_v<T, Real>)
+		{
+			return "Real";
+		}
+		else if constexpr (std::is_same_v<T, Point2d>)
+		{
+			return "Point2d";
+		}
+		else if constexpr (std::is_same_v<T, Segment>)
+		{
+			return "Segment";
+		}
+		else static_assert(false, "Unsupported type");
+	}
 
 };
 
