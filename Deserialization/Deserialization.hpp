42521b03e73c5faa26018f4f044d594e2d58b10e
Yupi <youngperchik2404@gmail.com>
2025-05-27 16:26:58 +0500

    Implemented deserialization methods

--- Deserialization/Deserialization.hpp
+++ Deserialization/Deserialization.hpp
@@ -6,6 +6,13 @@
 #include "../Point2d/Point2d.hpp"
 #include "../Segment/Segment.hpp"
 
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+
+
 template <typename T>
 
 class Deserialization
@@ -13,42 +20,81 @@ class Deserialization
 public:
 
     // Тут определяется формат десериализации
-	static T Deserialize(const std::string& data, const std::string& format);
+    static T Deserialize(const std::string& data, const std::string& form)
+    {
+        if (form == "txt")
+        {
+            return DeserializeFromTxt(data);
+        }
+        else if (form == "json")
+        {
+            return DeserializeFromJson(data);
+        }
+        else if (form == "xml")
+        {
+            return DeserializeFromXml(data);
+        }
+        else
+        {
+            throw std::invalid_argument("Unsupported format: " + form);
+        }
+    }
 
 private:
 
     // txt
-	static T DeserializeFromTxt(const std::string& data);
+    static T DeserializeFromTxt(const std::string& data)
+    {
+        T num;
+
+        std::istringstream iss(data);
+        iss >> num;
+
+        if (iss.fail()) 
+        {
+            throw std::runtime_error("Failed to parse number from TXT format");
+        }
+        return num;
+    }
+
 
     // json
-	static T DeserializeFromJson(const std::string& data);
+    static T DeserializeFromJson(const std::string& data)
+    {
+        size_t valuePos = data.find("\"value\":\"");
+
+        if (valuePos == std::string::npos) 
+        {
+            throw std::runtime_error("Invalid JSON format");
+        }
+
+        valuePos += 9; 
+        size_t valueEnd = data.find('"', valuePos);
+
+        if (valueEnd == std::string::npos) 
+        {
+            throw std::runtime_error("Invalid JSON format");
+        }
+
+        std::string value = data.substr(valuePos, valueEnd - valuePos);
+        return DeserializeFromTxt(value);
+    }
+
 
     // xml
-	static T DeserializeFromXml(const std::string& data);
+    static T DeserializeFromXml(const std::string& data)
+    {
+        size_t startTagEnd = data.find('>');
+        size_t endTagStart = data.find('<', startTagEnd + 1);
+
+        if (startTagEnd == std::string::npos || endTagStart == std::string::npos)
+        {
+            throw std::runtime_error("Invalid XML format");
+        }
+
+        std::string value = data.substr(startTagEnd + 1, endTagStart - startTagEnd - 1);
+        return DeserializeFromTxt(value);
+    }
 };
 
 
-
-
-// Тут определяется формат десериализации
-template<typename T>
-
-inline T Deserialization<T>::Deserialize(const std::string& data, const std::string& form)
-{
-    if (form == "txt")
-    {
-        return DeserializeFromTxt(data);
-    }
-    else if (form == "json")
-    {
-        return DeserializeFromJson(data);
-    }
-    else if (form == "xml")
-    {
-        return DeserializeFromXml(data);
-    }
-    else
-    {
-        throw std::invalid_argument("Unsupported format: " + form);
-    }
-}
--- Serialization/Serialization.hpp
+++ Serialization/Serialization.hpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <fstream>
 
+
 template <typename T>
 
 class Serialization
